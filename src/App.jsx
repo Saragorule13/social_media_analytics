27c3d848c98b788adfae04fdfb3e804b8eaf96fd
PrashantDhuri08 <prashantdhuripd6@gmail.com>
2025-01-08 23:23:25 +0530

    replaced ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import {Navbar} from './components/Navbar'
-import Main from './components/Main'
-import Input from './components/Input'
+import React from "react";
+import { Navbar } from "./components/Navbar";
+import Main from "./components/Main";
+import Input from "./components/Input";
+import Chat from "./components/Chats";
 
 function App() {
   return (
-    <div className='flex h-screen max-w-screen'>
-      <Navbar/>
+    <div className="flex h-screen max-w-screen">
+      <Navbar />
       <div>
-      <Main/>
-      <Input/>
+        {/* <Input/> */}
+        {/* <Main/> */}
+        {/* used chat component instead of Input and Main*/}
+        <Chat />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
